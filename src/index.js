a7f739027bc77059f33431c11ee16862543b1939
Viilhov <133661770+Viilhov@users.noreply.github.com>
2024-01-17 17:15:56 +0100

    Added basic JS form-handeling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipie(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipie-details", {
+    strings: "Soon you will see a recipie",
+    autoStart: true,
+    cursor: null,
+    delay: 50,
+  });
+}
+
+let submitIngredient = document.querySelector("#submit-ingredient-form");
+submitIngredient.addEventListener("submit", generateRecipie);

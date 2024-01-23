761c2437d44547c2f1b63045a7062817dd8e4a8b
Viilhov <133661770+Viilhov@users.noreply.github.com>
2024-01-23 14:53:40 +0100

    Changed the sub heading, updated css for recipie and added a generating recipie whilst the api are feching the info

--- src/index.js
+++ src/index.js
@@ -14,9 +14,11 @@ function generateRecipie(event) {
   let key = "331711t40ba7f6a03e3o8bfc5f54faa4";
   let prompt = `Generate a recipie using ${ingredientInput.value} as main ingredient`;
   let context =
-    "You are a recipie expert. You love to generate healty recipies with up to 6 easy to find ingrediants also providing user instructions. Please please display in basic HTML and seperate each ingredient and instructions with a <br>.";
+    "You are a recipie expert. You love to generate healty recipies with up to 6 easy to find ingrediants also providing user instructions. Please please display in basic HTML and seperate each ingredient and each instruction with a <br>.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
+  let recipieDisplay = document.querySelector("#recipie-details");
+  recipieDisplay.innerHTML = "Generating recipie";
   axios.get(apiUrl).then(showRecipie);
 }
 
